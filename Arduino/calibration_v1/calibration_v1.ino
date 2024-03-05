5ee1f3eb341627fcefd9b0d43244b6b0d8c85fbd
Alice <alice.duff5@gmail.com>
2024-03-05 15:48:55 -0500

    Deleted v2 and merged all changes into v1 file

--- Arduino/calibration_v1/calibration_v1.ino
+++ Arduino/calibration_v1/calibration_v1.ino
@@ -2,23 +2,28 @@
 // Basic demo for accelerometer readings from Adafruit LIS3DH
 
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
 
-// // Used for software SPI
-// #define LIS3DH_CLK 13
-// #define LIS3DH_MISO 12
-// #define LIS3DH_MOSI 11
-// // Used for hardware & software SPI
-// #define LIS3DH_CS 10
-
-// software SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
-// hardware SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
 // I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+
+
+// include the library code:
+#include "Adafruit_LiquidCrystal.h"
+
+// Connect via i2c, default address #0 (A0-A2 not jumpered)
+Adafruit_LiquidCrystal lcd(0);
+
+
+// for moving average
+int count = 0;
+int xarr[] = {0,0,0,0,0};
+int yarr[] = {0,0,0,0,0};
+int zarr[] = {0,0,0,0,0};
+
+
 
 void setup(void) {
   Serial.begin(115200);
@@ -32,7 +37,7 @@ void setup(void) {
   }
   Serial.println("LIS3DH found!");
 
-  lis.setRange(LIS3DH_RANGE_2_G);   // 2, 4, 8 or 16 G!
+  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
 
   Serial.print("Range = "); Serial.print(2 << lis.getRange());
   Serial.println("G");
@@ -52,26 +57,94 @@ void setup(void) {
     case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
     case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
   }
+
+
+
+
+
+
+
+  // set up the LCD's number of rows and columns:
+  if (!lcd.begin(16, 2)) {
+    Serial.println("Could not init backpack. Check wiring.");
+    while(1);
+  }
+  Serial.println("Backpack init'd.");
+
+
+
+  lcd.setCursor(0, 0);
+  lcd.print("Acceleration");
+
+
+
+
+
+
 }
 
 void loop() {
-  lis.read();      // get X Y and Z data at once
-  // Then print out the raw data
-  Serial.print("X:  "); Serial.print(lis.x);
-  Serial.print("  \tY:  "); Serial.print(lis.y);
-  Serial.print("  \tZ:  "); Serial.print(lis.z);
+  lis.read();      // get X Y and Z data
 
-  /* Or....get a new sensor event, normalized */
-  sensors_event_t event;
-  lis.getEvent(&event);
+    // // Check arrays
+    // Serial.print("Before copy: "); 
+    // for(int i = 0; i < 5; i++){Serial.print(xarr[i]); Serial.print(" ");}
+    // Serial.print("\t");
+    // for(int i = 0; i < 5; i++){Serial.print(yarr[i]); Serial.print(" ");}
+    // Serial.print("\t");
+    // for(int i = 0; i < 5; i++){Serial.print(zarr[i]); Serial.print(" ");}
+    // Serial.println();
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
-  Serial.print(" \tY: "); Serial.print(event.acceleration.y);
-  Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
-  Serial.println(" m/s^2 ");
 
+  int i;
+  for(i = 0; i <= 3; i++){
+    xarr[i] = xarr[i + 1];
+    yarr[i] = yarr[i + 1];
+    zarr[i] = zarr[i + 1];
+  }
+
+
+  xarr[4] = lis.x;
+  yarr[4] = lis.y;
+  zarr[4] = lis.z;
+
+
+  long sumx = 0;
+  long sumy = 0;
+  long sumz = 0;
+  for(i = 0; i < 5; i++){
+    sumx += xarr[i];
+    sumy += yarr[i];
+    sumz += zarr[i];
+  }
   Serial.println();
 
-  delay(200);
+  long x = sumx/5;
+  long y = sumy/5;
+  long z = sumz/5;
+  
+    Serial.print("X:  "); Serial.print(x);
+    Serial.print("  \tY:  "); Serial.print(y);
+    Serial.print("  \tZ:  "); Serial.print(x);
+    Serial.println();
+
+
+  lcd.setCursor(15, 0);
+  lcd.print(count);
+  count++;
+  if (count == 5){
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print(x/10);  // divide by 10 to truncate the least significant digit
+    lcd.setCursor(5, 1);
+    lcd.print(y/10);
+    lcd.setCursor(10, 1);
+    lcd.print(z/10);
+
+    count = 0;
+  }
+
+
+  delay(100);
 }
